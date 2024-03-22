20ea8b8e757dcfebdffc019fa0ed5040767e489f
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-03-22 08:03:13 -0500

    class pizza added

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,5 +1,11 @@
 namespace ModelsPizza.Models;
+
+
 public class Pizza
 {
-
+    //Atributos publicos
+    public int Id { get; set; }
+    public String? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+    
 }
